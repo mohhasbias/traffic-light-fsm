d3586f63343945151b32ece0e3c27b0838240db1
Moh Hasbi Assidiqi <mohhasbias@gmail.com>
2018-01-13 08:41:13 +0700

    use redux as store manager

--- index.js
+++ index.js
@@ -1,56 +1,14 @@
 // @flow
 // definition
-const createStore = (updater) => {
-  let _state = updater();
-  let _listeners = [];
+import JSM from 'javascript-state-machine';
+import { createStore as _createStore, combineReducers } from 'redux';
 
-  const getData = () => _state;
-  const subscribe = (listener) => _listeners.push(listener);
-  const dispatch = (action) => {
-    _state = updater(_state, action);
-    _listeners.forEach(l => l());
-  };
+const createStore = (...args) => _createStore(
+  ...args, 
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
-  return {
-    getData,
-    subscribe,
-    dispatch
-  }
-};
-
-const combineUpdater = (updatersObject) => {
-  return (state = {}, action) => Object.keys(updatersObject).reduce(
-    (nextState, key) => {
-      nextState[key] = updatersObject[key](state[key], action);
-      return nextState;
-    },
-    {}
-  );
-};
-
-const createFSM = (transitionRules) => {
-  let _currentState = '';
-  let _previousState = _currentState;
-  let _rules = transitionRules;
-
-  const begin = (state) => _currentState = state;
-  const getCurrentState = () => _currentState;
-  const getPreviousState = () => _previousState;
-  const doTransition = (...args) => {
-    _previousState = _currentState;
-    _currentState = _rules
-      .filter(rule => rule.from === _currentState)
-      .filter(rule => rule.when(...args))
-      .map(rule => rule.to)[0] || _currentState;
-  };
-
-  return {
-    begin,
-    doTransition,
-    getCurrentState,
-    getPreviousState,
-  };
-};
+const combineUpdater = combineReducers;
 
 // application
 
@@ -87,19 +45,18 @@ const trafficLightCounter = combineUpdater({
 });
 
 const store = createStore(trafficLightCounter);
+// rename to avoid confusion
+store.getData = store.getState;
 
 // fsm definition
-const redToYellowCondition = (data) => data.counter <= 0;
-const yellowToGreenCondition = (data) => data.counter <= 0;
-const greenToRedCondition = (data) => data.counter <= 0;
-
-const transitionRules = [
-  { from: 'red', to: 'yellow', when: redToYellowCondition },
-  { from: 'yellow', to: 'green', when: yellowToGreenCondition },
-  { from: 'green', to: 'red', when: greenToRedCondition }
-];
-
-const fsm = createFSM(transitionRules);
+const fsm = new JSM({
+  transitions: [
+    { name: 'init', from: 'none', to: 'red' },
+    { name: 'timer', from: 'red', to: (counter) => counter < 0 ? 'yellow' : 'red' },
+    { name: 'timer', from: 'yellow', to: (counter) => counter < 0 ? 'green' : 'yellow' },
+    { name: 'timer', from: 'green', to: (counter) => counter < 0 ? 'red' : 'green' },
+  ]
+});
 
 // render definition
 const render = (state, data) => {
@@ -112,49 +69,30 @@ const render = (state, data) => {
 // app maintenance
 store.subscribe(() => {
   // render ?
-  render(
-    {
-      prev: fsm.getPreviousState(),
-      current: fsm.getCurrentState()
-    }, 
-    store.getData()
-  );
-  console.log(fsm.getCurrentState(), store.getData());
-  fsm.doTransition(store.getData());
+  render({ current: fsm.state }, store.getData());
+  console.log(fsm.state, store.getData());
 });
 
+fsm.onAfterTransition = (lifecycle) => {
+  const { from, to } = lifecycle;
+  console.log(`${from} -> ${to}`);
+  if (from === 'red' && to === 'yellow') {
+    store.dispatch(counterResetAction(YELLOWCOUNTER_DURATION));
+  }
+  if (from === 'yellow' && to === 'green') {
+    store.dispatch(counterResetAction(GREENCOUNTER_DURATION));
+  }
+  if (from === 'green' && to === 'red') {
+    store.dispatch(counterResetAction(REDCOUNTER_DURATION));
+  }
+};
+
 // app start
-fsm.begin('red');
+fsm.init();
 store.dispatch(counterResetAction(REDCOUNTER_DURATION));
 
 // app trigger
 setInterval(() => {
-  const prevState = fsm.getPreviousState();
-  const currentState = fsm.getCurrentState();
-
-  console.log(`${prevState} -> ${currentState}`);
-
-  if (currentState === 'red') {
-    if (prevState === 'green') {
-      store.dispatch(counterResetAction(REDCOUNTER_DURATION));
-    } else {
-      store.dispatch(counterDecrementAction());
-    }
-  } 
-
-  if (currentState === 'yellow') {
-    if (prevState === 'red') {
-      store.dispatch(counterResetAction(YELLOWCOUNTER_DURATION));
-    } else {
-      store.dispatch(counterDecrementAction());
-    }
-  } 
-
-  if (currentState === 'green') {
-    if (prevState === 'yellow') {
-      store.dispatch(counterResetAction(GREENCOUNTER_DURATION));
-    } else {
-      store.dispatch(counterDecrementAction());
-    }
-  } 
+  store.dispatch(counterDecrementAction());
+  fsm.timer(store.getData().counter);
 }, 1000);
